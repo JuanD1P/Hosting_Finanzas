d5c80346ceb498c9298b4345b67dbc669de84550
Juan Penagos <juanndieg2005@gmail.com>
2025-05-03 10:15:17 -0500

    V3

--- Frontend/src/Components/Registro.jsx
+++ Frontend/src/Components/Registro.jsx
@@ -46,7 +46,7 @@ const Registro = () => {
         };
 
         try {
-            const result = await axios.post('https://hostingv1.onrender.com/auth/register', dataToSend);
+            const result = await axios.post('https://hosting-finanza.onrender.com/auth/register', dataToSend);
             if (result.data.registrationStatus) {
                 alert("Registro exitoso");
                 navigate('/userlogin');
